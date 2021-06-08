f2a1a138dcb3c62eb238c746902f93bd01ee6edc
LittleC <i@ltlec.cn>
2021-06-08 22:38:18 +0800

    :construction_worker: ci

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
+mkdir build
+cp index.html build/
 clang --target=wasm32 -Wl,--no-entry -Wl,--export-all -O3 -nostdlib -o build/main.wasm src/wasm.cpp -flto -Wl,--lto-O3 \
 -Wl,-z,stack-size=$[8 * 1024 * 1024] # Set maximum stack size to 8MiB
